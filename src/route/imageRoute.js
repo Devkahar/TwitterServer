dbbdeb3f3139f62a65e90c7aa40fe1309f6b1347
Devkahar <devkahar99@gmail.com>
2023-03-27 19:37:00 +0530

    env edit

--- src/route/imageRoute.js
+++ src/route/imageRoute.js
@@ -4,8 +4,8 @@ const multer = require("multer");
 const AWS = require("aws-sdk");
 const uuid = require("uuid");
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID_APP,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_APP,
 });
 const upload = multer().single("image");
 router.post("/image/upload", upload, (req, res, next) => {
@@ -13,7 +13,7 @@ router.post("/image/upload", upload, (req, res, next) => {
   const fileType = myFile[myFile.length - 1];
   console.log("Bucket Name ", process.env.AWS_BUCKET_NAME);
   const params = {
-    Bucket: process.env.AWS_BUCKET_NAME,
+    Bucket: process.env.AWS_BUCKET_NAME_APP,
     Key: `${uuid.v4()}.${fileType}`,
     Body: req.file.buffer,
   };
